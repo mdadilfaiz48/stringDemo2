d62494a36cf16bf1786e9e978b120e483ab395aa
mdadilfaiz48 <mdadilfaiz48@gmail.com>
2025-09-23 03:33:14 -0700

    added and update 9-23-2025

---
+++ src/MyTest.java
@@ -0,0 +1,34 @@
+public class MyTest {
+
+    public static void main(String[] args) {
+//        StringBuilder sb = new StringBuilder(5);
+//        String s = "";
+//        if(sb.equals(s)){
+//            System.out.println("match 1");
+//        }
+//        else if(sb.toString().equals(s.toString()))
+//        {
+//            System.out.println("match 2");
+//        }
+//        else
+//        {
+//               System.out.println("no match");
+//        }
+
+//        StringBuilder sb1 = new StringBuilder("durga");
+//        String str1 = sb1.toString();
+//        String str2 = str1;  // true
+//        String str2 = new String(str1);// false
+//        String str2 = sb1.toString();false
+//        String str2 = "durga";// false
+//        System.out.println(str1 == str2);
+
+//
+//        StringBuilder sb = new StringBuilder("durga");
+//        sb.delete(0,sb.length());
+//        System.out.println(sb.length());
+
+
+
+    }
+}
